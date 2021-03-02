8056d4c3c69323a13b0b137a966e1cc3136ebb84
UndefinedOffset <echipman@webbuildersgroup.com>
2021-03-02 10:07:10 -0400

    Fixed failing tests

--- tests/forms/gridfield/GridFieldDeletedTest.php
+++ tests/forms/gridfield/GridFieldDeletedTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace WebbuildersGroup\GridFieldDeletedItems\Tests;
 
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\Session;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\Dev\TestOnly;
 use SilverStripe\Forms\FieldList;
@@ -10,15 +14,11 @@ use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use SilverStripe\Forms\GridField\GridFieldDataColumns;
 use SilverStripe\Forms\GridField\GridFieldEditButton;
 use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
-use SilverStripe\Control\Controller;
-use SilverStripe\Control\Session;
-use SilverStripe\Control\HTTPRequest;
-use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Versioned\Versioned;
 use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedColumns;
-use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedEditButton;
 use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedDeleteAction;
+use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedEditButton;
 use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedManipulator;
 use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedRestoreButton;
 use WebbuildersGroup\GridFieldDeletedItems\Forms\GridFieldDeletedToggle;
@@ -27,29 +27,29 @@ class GridFieldDeletedTest extends FunctionalTest
 {
     protected static $fixture_file = 'GridFieldDeletedTest.yml';
     protected static $extra_dataobjects = [GridFieldDeletedTest\TestObject::class];
-    
+
     protected $list;
     protected $gridField;
     protected $form;
-    
+
     public function setUp()
     {
         parent::setUp();
-        
+
         $this->list = GridFieldDeletedTest\TestObject::get();
         $config = GridFieldConfig_RecordEditor::create(10)
-                                                        ->removeComponentsByType(GridFieldDataColumns::class)
-                                                        ->removeComponentsByType(GridFieldEditButton::class)
-                                                        ->addComponent(new GridFieldDeletedManipulator(), GridFieldToolbarHeader::class)
-                                                        ->addComponent(new GridFieldDeletedColumns())
-                                                        ->addComponent(new GridFieldDeletedEditButton())
-                                                        ->addComponent(new GridFieldDeletedRestoreButton())
-                                                        ->addComponent(new GridFieldDeletedToggle('buttons-before-left'));
-        
+            ->removeComponentsByType(GridFieldDataColumns::class)
+            ->removeComponentsByType(GridFieldEditButton::class)
+            ->addComponent(new GridFieldDeletedManipulator(), GridFieldToolbarHeader::class)
+            ->addComponent(new GridFieldDeletedColumns())
+            ->addComponent(new GridFieldDeletedEditButton())
+            ->addComponent(new GridFieldDeletedRestoreButton())
+            ->addComponent(new GridFieldDeletedToggle('buttons-before-left'));
+
         $this->gridField = new GridField('testfield', 'testfield', $this->list, $config);
         $this->form = new Form(new GridFieldDeletedTest\DummyController(), 'mockform', new FieldList([$this->gridField]), new FieldList());
     }
-    
+
     /**
      * Tests toggling the list on and off
      */
@@ -63,40 +63,40 @@ class GridFieldDeletedTest extends FunctionalTest
                 $item->delete();
             }
         }
-        
-        
+
+
         //In the default state the list should not contain the deleted items
         $this->assertEquals(0, $this->gridField->getManipulatedList()->filter('ID', $deletedIDs)->count(), 'Deleted items are visible in the list and they should not be in the default state');
-        
-        
+
+
         //Toggle the show deleted on
         $stateID = 'testGridStateActionField';
         $request = new HTTPRequest('POST', 'url', [], ['action_gridFieldAlterAction?StateID=' . $stateID => true, $this->form->getSecurityToken()->getName() => $this->form->getSecurityToken()->getValue()]);
-        $session = Injector::inst()->create(Session::class, []);
+        $session = Controller::curr()->getRequest()->getSession();
+        $session->set($this->form->getSecurityToken()->getName(), $this->form->getSecurityToken()->getValue());
+        $session->set($stateID, ['grid' => '', 'actionName' => 'gf-toggle-deleted', 'args' => ['ListDisplayMode' => ['ShowDeletedItems' => 'Y']]]);
         $request->setSession($session);
-        $session->init($request);
-        $request->getSession()->set($stateID, ['grid' => '', 'actionName' => 'gf-toggle-deleted', 'args' => ['ListDisplayMode' => ['ShowDeletedItems' => 'Y']]]);
         $this->gridField->gridFieldAlterAction(['StateID' => $stateID], $this->form, $request);
-        
-        
+
+
         //Check to see if the deleted items are now visible
         $this->assertGreaterThan(0, $this->gridField->getManipulatedList()->filter('ID', $deletedIDs)->count(), 'Deleted items are not visible in the list and they should be when the toggle is on');
-        
-        
+
+
         //Toggle the show deleted back off
         $stateID = 'testGridStateActionField';
         $request = new HTTPRequest('POST', 'url', [], ['action_gridFieldAlterAction?StateID=' . $stateID => true, $this->form->getSecurityToken()->getName() => $this->form->getSecurityToken()->getValue()]);
-        $session = Injector::inst()->create(Session::class, []);
+        $session = Controller::curr()->getRequest()->getSession();
+        $session->set($this->form->getSecurityToken()->getName(), $this->form->getSecurityToken()->getValue());
+        $session->set($stateID, ['grid' => '', 'actionName' => 'gf-toggle-deleted', 'args' => ['ListDisplayMode' => ['ShowDeletedItems' => 'N']]]);
         $request->setSession($session);
-        $session->init($request);
-        $request->getSession()->set($stateID, ['grid' => '', 'actionName' => 'gf-toggle-deleted', 'args' => ['ListDisplayMode' => ['ShowDeletedItems' => 'N']]]);
         $this->gridField->gridFieldAlterAction(['StateID' => $stateID], $this->form, $request);
-        
-        
+
+
         //Check to see if the deleted items are now visible
         $this->assertEquals(0, $this->gridField->getManipulatedList()->filter('ID', $deletedIDs)->count(), 'Deleted items are visible in the list and they should be when the toggle is off');
     }
-    
+
     /**
      * Tests restoring a deleted item
      */
@@ -106,28 +106,28 @@ class GridFieldDeletedTest extends FunctionalTest
         $deletedItem = $this->objFromFixture(GridFieldDeletedTest\TestObject::class, 'testobj2');
         $deletedItemID = $deletedItem->ID;
         $deletedItem->delete();
-        
-        
+
+
         //Make sure the item was deleted
         $this->assertNull(Versioned::get_one_by_stage(GridFieldDeletedTest\TestObject::class, 'Stage', '"ID"=' . $deletedItemID), 'Item was not deleted prior to restoring');
-        
-        
+
+
         //Attempt to restore the item
         $stateID = 'testGridStateActionField';
         $request = new HTTPRequest('POST', 'url', [], ['action_gridFieldAlterAction?StateID=' . $stateID => true, $this->form->getSecurityToken()->getName() => $this->form->getSecurityToken()->getValue()]);
-        $session = Injector::inst()->create(Session::class, []);
+        $session = Controller::curr()->getRequest()->getSession();
+        $session->set($this->form->getSecurityToken()->getName(), $this->form->getSecurityToken()->getValue());
+        $session->set($stateID, ['grid' => '', 'actionName' => 'restore-draft-item', 'args' => ['RecordID' => $deletedItemID]]);
         $request->setSession($session);
-        $session->init($request);
-        $request->getSession()->set($stateID, ['grid' => '', 'actionName' => 'restore-draft-item', 'args' => ['RecordID' => $deletedItemID]]);
         $this->gridField->gridFieldAlterAction(['StateID' => $stateID], $this->form, $request);
-        
-        
+
+
         //Check to see if the item exists again
         $item = Versioned::get_one_by_stage(GridFieldDeletedTest\TestObject::class, 'Stage', '"Title"=\'Test Object 2\'');
         $this->assertInstanceOf(GridFieldDeletedTest\TestObject::class, $item, 'Could not find the item after restoring');
         $this->assertTrue($item->exists(), 'Could not find the item after restoring');
     }
-    
+
     /**
      * Tests if the edit button is removed for deleted items or not
      */
@@ -136,35 +136,35 @@ class GridFieldDeletedTest extends FunctionalTest
         //Load the item to delete and capture it's id then delete it
         $deletedItem = $this->objFromFixture(GridFieldDeletedTest\TestObject::class, 'testobj2');
         $deletedItem->delete();
-        
-        
+
+
         //Enable the deleted items
         $this->gridField->getState()->ListDisplayMode->ShowDeletedItems = 'Y';
-        
-        
+
+
         //Get the attributes for the deleted item's title column
         $attributes = $this->gridField->getConfig()->getComponentByType(GridFieldDeletedColumns::class)->getColumnAttributes($this->gridField, $deletedItem, 'Title');
-        
-        
+
+
         //Verify we have an array and the class attribute exists
         $this->assertInternalType('array', $attributes);
         $this->assertArrayHasKey('class', $attributes);
-        
-        
+
+
         //Verify that the deleted-record class is applied
         $classes = explode(' ', $attributes['class']);
         $this->assertContains('deleted-record', $classes, 'Item was deleted but could not find the deleted-record class');
-        
-        
+
+
         //Get the attributes for a non-deleted item
         $attributes = $this->gridField->getConfig()->getComponentByType(GridFieldDeletedColumns::class)->getColumnAttributes($this->gridField, $this->objFromFixture(GridFieldDeletedTest\TestObject::class, 'testobj1'), 'Title');
-        
-        
+
+
         //Verify we have an array and the class attribute exists
         $this->assertInternalType('array', $attributes);
         $this->assertArrayHasKey('class', $attributes);
-        
-        
+
+
         //Verify that the deleted-record class is not applied
         $classes = explode(' ', $attributes['class']);
         $this->assertNotContains('deleted-record', $classes, 'Item was not deleted but the deleted-record class was found');
--- tests/forms/gridfield/GridFieldDeletedTest/TestObject.php
+++ tests/forms/gridfield/GridFieldDeletedTest/TestObject.php
@@ -1,20 +1,20 @@
 <?php
 namespace WebbuildersGroup\GridFieldDeletedItems\Tests\GridFieldDeletedTest;
 
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Versioned\Versioned;
 use WebbuildersGroup\GridFieldDeletedItems\Tests\GridFieldDeletedTest_TestObject;
-use SilverStripe\ORM\DataObject;
-use SilverStripe\Dev\TestOnly;
 
 class TestObject extends DataObject implements TestOnly
 {
     private static $db = [
-                            'Title' => 'Varchar(255)',
-                        ];
-    
+        'Title' => 'Varchar(255)',
+    ];
+
     private static $extensions = [
-                                    Versioned::class
-                                ];
-    
+        Versioned::class,
+    ];
+
     private static $table_name = 'GridFieldDeletedTest_TestObject';
 }
